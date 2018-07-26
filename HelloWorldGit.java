0b1ca9928adeadc00c2a4e2603727109363be353
suraj ranabhat <suraj_ranabhat@persistent.co.in>
2018-07-26 15:11:38 +0530

    display date method called by object

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -7,5 +7,10 @@ class HelloWorld{
 		public static void main(String[] args){
 
 		System.out.println("helloworld...Welcome to Git");
+
+
+		HelloWord obj = new HelloWorld();
+		obj.displaDate();
+
 		}
 	}
